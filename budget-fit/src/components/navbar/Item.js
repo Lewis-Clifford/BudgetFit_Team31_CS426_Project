df180229042346272f0ef1365ecb13d72e5689c3
Lewis-Clifford <clifflewis21@gmail.com>
2022-12-07 17:00:49 -0800

    Filled Out Item Class

--- budget-fit/src/components/navbar/Item.js
+++ budget-fit/src/components/navbar/Item.js
@@ -7,6 +7,44 @@ class Item extends Component {
         cost: 0.00,
         store: ""
      };
+
+    constructor(name, imageUrl, cost, store) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.cost = cost;
+        this.store = store;
+    }
+    setName(newName) {
+        this.name = newName;
+    }
+
+    setImageUrL(newUrl) {
+        this.imageUrl = newUrl;
+    }
+
+    setCost(newCost) {
+        this.cost = newCost;
+    }
+
+    setStore(newStore) {
+        this.store = newStore;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getImageUrl() {
+        return this.imageUrl;
+    }
+
+    getCost() {
+        return this.cost;
+    }
+
+    getStore() {
+        return this.store;
+    }
     render() { 
         return ;
     }
