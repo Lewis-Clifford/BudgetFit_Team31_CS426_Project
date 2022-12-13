24108e06e9b7770bc6650b8d0fef33f73a68ffa6
aramprogram <akliasian85.steam@gmail.com>
2022-12-13 11:59:08 -0800

    diet and exercise updated

--- src/BudgetFitPages/diet.js
+++ src/BudgetFitPages/diet.js
@@ -12,34 +12,41 @@ const DietPage = () => {
     <NavBar />
     <div className="centered-div">
         <div class="container">
-        <div class="box ">
-            <div class="cus-pad ">
-            <div class="cus-main">
-                <h3 class="center1">What are your seconday diet goals?</h3>
-                <h6 class="center1">*Choose unlimited from Part 2, besides last option</h6>
-                <br>
-                <h3 class="center1">Part 1</h3>
-                <form>
-                <input type="radio" id="keto" name="diet_type" value="keto-diet">
-                <label for="keto">Keto Diet (Low-no carbohydrates)</label><br>
-                <input type="radio" id="veget" name="diet_type" value="veget-diet">
-                <label for="keto">Vegetarian Diet (No meat)</label><br>
-                <input type="radio" id="vegab" name="diet_type" value="vegan-diet">
-                <label for="keto">Vegan Diet (No meat or dairy)</label><br>
-                <input type="radio" id="carni" name="diet_type" value="carni-diet">
-                <label for="keto">Carnivore Diet (Strictly animal based)</label><br>
-                <input type="radio" id="paleo" name="diet_type" value="paleo-diet">
-                <label for="keto">Paleo Diet (Strictly whole foods, avoid processed)</label><br>
-                <input type="radio" id="none" name="diet_type" value="no-diet">
-                <label for="keto">I <strong>do not</strong> plan on eating any of these diets</label><br>
-                <br>
-                <br>
-                <br>
-                <button type="submit" class="btn btn-primary custom-btn"> FINISH </button>
+            <div class="box ">
+                <div class="cus-pad ">
+                    <div class="cus-main">
+                        <h3 class="center1">What are your seconday diet goals?</h3>
+                        <h6 class="center1">*Choose unlimited from Part 2, besides last option</h6>
+                        <br>
+                        <h3 class="center1">Part 1</h3>
+                        <form>
+                            <input type="radio" id="keto" name="diet_type" value="keto-diet">
+                                <label for="keto">Keto Diet (Low-no carbohydrates)</label>
+                                <br>
+                            <input type="radio" id="veget" name="diet_type" value="veget-diet">
+                                <label for="keto">Vegetarian Diet (No meat)</label>
+                                <br>
+                            <input type="radio" id="vegab" name="diet_type" value="vegan-diet">
+                                <label for="keto">Vegan Diet (No meat or dairy)</label>
+                                <br>
+                            <input type="radio" id="carni" name="diet_type" value="carni-diet">
+                                <label for="keto">Carnivore Diet (Strictly animal based)</label>
+                                <br>
+                            <input type="radio" id="paleo" name="diet_type" value="paleo-diet">
+                                <label for="keto">Paleo Diet (Strictly whole foods, avoid processed)</label>
+                                <br>
+                            <input type="radio" id="none" name="diet_type" value="no-diet">
+                                <label for="keto">I <strong>do not</strong> plan on eating any of these diets</label>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                            <button type="submit" class="btn btn-primary custom-btn"> FINISH </button>
+                        </form>
+                    </div>
+                    <h3 class="center1">Need help?</h3>
+                </div>
             </div>
-            <h3 class="center1">Need help?</h3>
-            </div>
-        </div>
         </div>
     </div>
     </div>
