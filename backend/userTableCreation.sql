8e57f1a663122ceba7c14a51dcb6ecc5e5317073
Lewis-Clifford <clifflewis21@gmail.com>
2023-02-17 15:34:54 -0800

    Script for Creating User Table

---
+++ backend/userTableCreation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE
+    db_budgetfit.user (
+        userID int NOT NULL AUTO_INCREMENT,
+        userName varchar(100) DEFAULT NULL,
+        email varchar(100) DEFAULT NULL,
+        password varbinary(100) DEFAULT NULL,
+        activeUser tinyint(1) DEFAULT NULL,
+        modifiedDate datetime DEFAULT NULL,
+        createdDate datetime DEFAULT NULL,
+        PRIMARY KEY (userID),
+        UNIQUE INDEX id (userID)
+    ) ENGINE = INNODB,
+    AUTO_INCREMENT = 2,
+    CHARACTER SET utf8mb4,
+    COLLATE utf8mb4_0900_ai_ci;
