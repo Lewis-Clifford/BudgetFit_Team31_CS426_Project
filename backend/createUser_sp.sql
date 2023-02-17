6a3435ddca225c843bfb9e0433070b2becc7cf8f
Lewis-Clifford <clifflewis21@gmail.com>
2023-02-17 15:40:03 -0800

    Stored Procedure

---
+++ backend/createUser_sp.sql
@@ -0,0 +1,21 @@
+CREATE DEFINER =`CLIFF`@`%` PROCEDURE `CREATEUSER`(
+IN USERNAME VARCHAR(100), IN EMAIL VARCHAR(100), IN 
+PASSWORD VARBINARY(100)) BEGIN 
+	INSERT INTO
+	    USER (
+	        userName,
+	        email,
+	        password,
+	        activeUser,
+	        modifiedDate,
+	        createdDate
+	    )
+	VALUES (
+	        userName,
+	        email,
+	        password,
+	        1,
+	        NOW(),
+	        NOW()
+	    );
+	END 
