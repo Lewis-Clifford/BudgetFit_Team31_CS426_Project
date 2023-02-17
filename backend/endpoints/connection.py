4b99dfc9b88e89c777d2edb74b4da4db01db2ac9
Lewis-Clifford <clifflewis21@gmail.com>
2023-02-17 00:08:58 -0800

    Achieved Frontend to Database Connection for Register Endpoint POST Request. Requires stored procedure and existing table in MySQL database.

--- backend/endpoints/connection.py
+++ backend/endpoints/connection.py
@@ -4,44 +4,42 @@ from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey #unused imports left in for later reference
 from sqlalchemy.dialects.mysql import VARBINARY, VARCHAR, BOOLEAN, DATETIME
 from sqlalchemy.sql import text
-from sqlalchemy import inspect, URL
+from sqlalchemy import URL
 from datetime import datetime
+from mysql.connector import MySQLConnection, Error
+
 
 
 app = Flask(__name__)
 url = URL.create("mysql", username='cliff', password='PervertCamel21@@',host='localhost',database='db_budgetfit') #Connection string
 app.config['SQLALCHEMY_DATABASE_URI'] = url 
 
-metadata = MetaData()
 
-user = Table('user', metadata, 
-    Column('id', Integer, unique=True, primary_key=True, autoincrement=True, ),
-    Column('email', VARCHAR(100), unique=True),
-    Column('password', VARBINARY(100)),
-    Column('activeUser', BOOLEAN),
-    Column('modifiedDate', DATETIME),
-    Column('createdDate', DATETIME)
-)
 
-engine = create_engine(url)
-metadata.create_all(engine)
 
-'''
-with engine.connect() as con:
+
+
+db_engine = create_engine(url)
     
-    testData = ({"id":1,"email": "test@gmail.com", "password": 0, "activeUser": True, "modifiedDate": datetime.now(), "createdDate": datetime.now()},
-    {"id":2,"email": "test@gmail.com", "password": 0, "activeUser": True, "modifiedDate": datetime.now(), "createdDate": datetime.now()}
-    )
 
-    input = text("INSERT INTO user(id, email, password, activeUser, modifiedDate, createdDate) VALUES (:id, :email,:password,:activeUser,:modifiedDate,:createdDate)")
+input = {"email": "test@gmail.com", "password": None}
+    
+con = db_engine.engine.raw_connection()
 
-    for line in testData:
-        con.execute(input, **line)
 
-'''
-with engine.connect() as con:
+### Call stored procedure
+try:
+    cursor = con.cursor()
+    cursor.callproc('createUser', input.values())
+    cursor.close()
+    con.commit()   
+finally:
+    con.close()     
 
-    table = con.execute('SELECT * FROM user')
+
+with db_engine.connect() as con:
+
+    table = con.execute(text('SELECT * FROM user'))
 
     for record in table:
         print(record)
@@ -51,5 +49,5 @@ with engine.connect() as con:
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
     
--- backend/endpoints/register_endpoint.py
+++ backend/endpoints/register_endpoint.py
@@ -2,8 +2,14 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from sqlalchemy import URL
+from sqlalchemy import create_engine
 
 register = Flask(__name__)
+
+url = URL.create("mysql", username='cliff', password='PervertCamel21@@',host='localhost',database='db_budgetfit')   #Connection string. Will be different per local database.
+register.config['SQLALCHEMY_DATABASE_URI'] = url                                                                    #Connect SQLAlchemy to Database
+db_engine = create_engine(url)                                                                                      #Create Engine to Use Database
 
 cors = CORS(register)
 
@@ -26,13 +32,24 @@ def registerGet():
 
 def registerPost():
     
+    con = db_engine.engine.raw_connection()                     #Open database connection
+
     try:
         registerForm = request.get_json()
-        print(registerForm)
+
+        cursor = con.cursor()                                   #Create connection cursor
+        cursor.callproc('createUser', registerForm.values())    #This is the call to the stored procedure
+        cursor.close()                                          #Close connection cursor
+        con.commit()                                            #Commit changes to update database
+
+        
         return jsonify(registerForm)
 
     except:
-        return 'Failed', 400
+        return '', 400
+    
+    finally:
+        con.close()                                             #Close database connection
 
 @register.route('/register', methods=['DELETE'])
 
