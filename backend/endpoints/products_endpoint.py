a4bd983893082e509ea31a74e49f8ba0998da1fb
Kaden Nesch <70534010+Kaden-Nesch@users.noreply.github.com>
2023-03-13 10:51:12 -0700

    Add files via upload

--- backend/endpoints/products_endpoint.py
+++ backend/endpoints/products_endpoint.py
@@ -2,15 +2,10 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from sqlalchemy import create_engine
 from sqlalchemy import text
-from sqlalchemy import URL
 
 products = Flask(__name__)
 
-
-
-#url = URL.create('mysql', username='cliff', password='PervertCamel21@@', host='localhost', database='db_budgetfit')
-#products.config['SQLALCHEMY_DATABASE_URI'] = url
-url = "mysql://Kaden:Dnss7564$$@localhost/db_budgetfit"
+url = "mysql://root:Dnss7564$$@localhost/budgetfit"
 products.config['SQLALCHEMY_DATABASE_URI'] = url                                                                    
 db_engine = create_engine(url)                                                                                      
 
@@ -24,16 +19,31 @@ products.config['CORS_HEADERS'] = 'Content-Type'
 
 @products.route('/products', methods=['GET'])
 def get_products():
-    params = request.get_json()
-    with db_engine.connect() as con:  # Open connection
-        cursor = con.cursor()
-        rs = cursor.callproc('pagination', [params.values()[0], params.values()[1]])
-        productsForm = []
-        for row in rs:
-            item_name, item_price, images_front_full_url = row
-            product = {'item_name': item_name, 'item_price': item_price, 'images_front_full_url': images_front_full_url}
-            productsForm.append(product)
-        return jsonify(productsForm)
+    limit = request.args.get('limit', default=9, type=int)
+    page_number = request.args.get('page', default=1, type=int)
+    print('working')
+    con = db_engine.engine.raw_connection()
+    cursor = con.cursor()                                   #Create connection cursor
+    cursor.callproc('pagination', [limit, page_number])    #This is the call to the stored procedure
+
+    productsForm = []
+    rs = cursor.fetchall()
+
+    field_names = [d[0] for d in cursor.description]
+
+    for row in rs:
+        row_dict = {}
+        for i, value in enumerate(row):
+            row_dict[field_names[i]] = value
+
+        brand_name, item_name, item_price, images_front_full_url = row_dict['brand_name'], row_dict['item_name'], row_dict['item_price'], row_dict['images_front_full_url']
+        product = {'brand_name': brand_name,'item_name': item_name, 'item_price': item_price, 'images_front_full_url': images_front_full_url}
+        productsForm.append(product)
+
+    cursor.close()                                          #Close connection cursor
+    con.commit()                                            #Commit changes to update database
+    con.close()
+    return jsonify(productsForm)
 
 if __name__ == '__main__':
     products.run(debug=True)
