f90426a67274a5c764ceb40bddb0b0cb7275540f
Lewis-Clifford <clifflewis21@gmail.com>
2023-04-20 17:26:32 -0700

    Added Diet Fields and updated to use userID instead of personID

--- backend/storedProcedures/createUserPlus_sp.sql
+++ backend/storedProcedures/createUserPlus_sp.sql
@@ -4,62 +4,70 @@ IN PASSWORD VARBINARY(255)) BEGIN
 	declare tempUserID int;
 	DECLARE tempPersonID int;
 	START TRANSACTION;
-	INSERT INTO
-	    USER (
-	        userName,
-	        email,
-	        PASSWORD,
-	        activeUser,
-	        modifiedDate,
-	        createdDate
-	    )
-	VALUES (
-	        userName,
-	        email,
-	        password,
-	        1,
-	        NOW(),
-	        NOW()
-	    );
-	SELECT LAST_INSERT_ID() INTO tempUserID;
-	INSERT INTO
-	    person (
-	        userId,
-	        isActive,
-	        createdDate,
-	        modifiedDate
-	    )
-	VALUES (tempUserID, 0, NULL, NULL);
-	INSERT INTO
-	    personform (
-	        personID,
-	        name,
-	        gender,
-	        phoneNumber,
-	        weight,
-	        birthday,
-	        heightFeet,
-	        heightInches,
-	        workoutExp,
-	        fitnessLevel,
-	        isActive,
-	        createdDate,
-	        modifiedDate
-	    )
-	VALUES (
-	        tempUserID,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        NULL,
-	        0,
-	        NULL,
-	        NULL
-	    );
-	COMMIT;
-	END 
+
+INSERT INTO
+    USER (
+        userName,
+        email,
+        PASSWORD,
+        activeUser,
+        modifiedDate,
+        createdDate
+    )
+VALUES (
+        userName,
+        email,
+        password,
+        1,
+        NOW(),
+        NOW()
+    );
+
+SELECT LAST_INSERT_ID() INTO tempUserID;
+INSERT INTO
+    person (
+        userId,
+        isActive,
+        createdDate,
+        modifiedDate
+    )
+VALUES (tempUserID, 0, NULL, NULL);
+INSERT INTO
+    personform (
+        userID,
+        name,
+        gender,
+        phoneNumber,
+        weight,
+        birthday,
+        heightFeet,
+        heightInches,
+        workoutExp,
+        fitnessLevel,
+        allergy,
+        diet,
+        isActive,
+        createdDate,
+        modifiedDate
+    )
+VALUES (
+        tempUserID,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        0,
+        NULL,
+        NULL
+    );
+
+COMMIT;
+
+END 
