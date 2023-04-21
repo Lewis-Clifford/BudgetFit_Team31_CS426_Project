f3c7c7d4b44dbc71a4bceb733dea25933168ef4d
Lewis-Clifford <clifflewis21@gmail.com>
2023-04-20 17:29:04 -0700

    Changed personID FK field to userID FK

--- backend/databaseCreation/personFormTableCreation.sql
+++ backend/databaseCreation/personFormTableCreation.sql
@@ -1,23 +1,23 @@
 CREATE TABLE
     db_budgetfit.personform (
         personFormID INT NOT NULL AUTO_INCREMENT,
-        personID INT DEFAULT NULL COMMENT 'Foreign Key',
-        heightFeet INT DEFAULT NULL,
-        heightInches INT DEFAULT NULL,
-        birthday DATE DEFAULT NULL,
-        phoneNumber VARCHAR(20) DEFAULT NULL,
-        gender VARCHAR(100) DEFAULT NULL,
-        weight DECIMAL(8, 2) DEFAULT NULL,
-        workoutExp INT DEFAULT NULL,
-        fitnessLevel INT DEFAULT NULL,
-        createdDate DATETIME DEFAULT NULL,
-        modifiedDate DATETIME DEFAULT NULL,
-        isActive TINYINT(1) DEFAULT NULL,
-        name VARCHAR(100) DEFAULT NULL,
-        allergy VARCHAR(255) DEFAULT NULL,
-        diet VARCHAR(255) DEFAULT NULL,
-        PRIMARY KEY (personFormID)
-) ENGINE = INNODB, AUTO_INCREMENT = 3, 
+userID INT DEFAULT NULL COMMENT 'Foreign Key',
+heightFeet INT DEFAULT NULL,
+heightInches INT DEFAULT NULL,
+birthday DATE DEFAULT NULL,
+phoneNumber VARCHAR(20) DEFAULT NULL,
+gender VARCHAR(100) DEFAULT NULL,
+weight DECIMAL(8, 2) DEFAULT NULL,
+workoutExp INT DEFAULT NULL,
+fitnessLevel INT DEFAULT NULL,
+createdDate DATETIME DEFAULT NULL,
+modifiedDate DATETIME DEFAULT NULL,
+isActive TINYINT(1) DEFAULT NULL,
+name VARCHAR(100) DEFAULT NULL,
+allergy VARCHAR(255) DEFAULT NULL,
+diet VARCHAR(255) DEFAULT NULL,
+PRIMARY KEY (personFormID)
+) ENGINE = INNODB, AUTO_INCREMENT = 3, AVG_ROW_LENGTH = 16384, 
 CHARACTER SET utf8mb4,
 COLLATE utf8mb4_0900_ai_ci,
 COMMENT = 'Reference Table: Getters Only';
@@ -25,4 +25,4 @@ COMMENT = 'Reference Table: Getters Only';
 ALTER TABLE
     db_budgetfit.personform
 ADD
-    CONSTRAINT FK_personform_personID FOREIGN KEY (personID) REFERENCES db_budgetfit.person(personID);
+    CONSTRAINT FK_personform_userID FOREIGN KEY (userID) REFERENCES db_budgetfit.user(userID);
