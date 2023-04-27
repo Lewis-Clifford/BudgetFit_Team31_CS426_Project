21eae10e98ba4e63a721c41b7dd051ffd7b0aafd
Kaden Nesch <70534010+Kaden-Nesch@users.noreply.github.com>
2023-04-27 12:22:18 -0700

    Updated endpoint for master search

--- backend/endpoints/products_endpoint.py
+++ backend/endpoints/products_endpoint.py
@@ -38,8 +38,38 @@ def get_products():
         for i, value in enumerate(row):
             row_dict[field_names[i]] = value
 
-        brand_name, item_name, item_price, images_front_full_url = row_dict['brand_name'], row_dict['item_name'], row_dict['item_price'], row_dict['images_front_full_url']
-        product = {'brand_name': brand_name,'item_name': item_name, 'item_price': item_price, 'images_front_full_url': images_front_full_url}
+        productsID, brand_name, item_category, item_name, nf_ingredient_statement, nf_calories, nf_calories_from_fat, nf_total_fat, nf_saturated_fat, nf_trans_fatty_acid, nf_polyunsaturated_fat, nf_monounsaturated_fat, nf_cholesterol, nf_sodium, nf_total_carbohydrate, nf_dietary_fiber, nf_sugars, nf_protein, nf_vitamin_a_dv, nf_vitamin_c_dv, nf_calcium_dv, nf_iron_dv, nf_potassium, nf_servings_per_container, nf_serving_size_qty, nf_serving_size_unit, nf_serving_weight_grams, metric_qty, metric_uom, images_front_full_url, item_price = row_dict['productsID'], row_dict['brand_name'], row_dict['item_category'], row_dict['item_name'], row_dict['nf_ingredient_statement'], row_dict['nf_calories'], row_dict['nf_calories_from_fat'], row_dict['nf_total_fat'], row_dict['nf_saturated_fat'], row_dict['nf_trans_fatty_acid'], row_dict['nf_polyunsaturated_fat'], row_dict['nf_monounsaturated_fat'], row_dict['nf_cholesterol'], row_dict['nf_sodium'], row_dict['nf_total_carbohydrate'], row_dict['nf_dietary_fiber'], row_dict['nf_sugars'], row_dict['nf_protein'], row_dict['nf_vitamin_a_dv'], row_dict['nf_vitamin_c_dv'], row_dict['nf_calcium_dv'], row_dict['nf_iron_dv'], row_dict['nf_potassium'], row_dict['nf_servings_per_container'], row_dict['nf_serving_size_qty'], row_dict['nf_serving_size_unit'], row_dict['nf_serving_weight_grams'], row_dict['metric_qty'], row_dict['metric_uom'], row_dict['images_front_full_url'], row_dict['item_price']
+        product = {'productsID': productsID,
+           'brand_name': brand_name,
+           'item_category': item_category,
+           'item_name': item_name,
+           'nf_ingredient_statement': nf_ingredient_statement,
+           'nf_calories': nf_calories,
+           'nf_calories_from_fat': nf_calories_from_fat,
+           'nf_total_fat': nf_total_fat,
+           'nf_saturated_fat': nf_saturated_fat,
+           'nf_trans_fatty_acid': nf_trans_fatty_acid,
+           'nf_polyunsaturated_fat': nf_polyunsaturated_fat,
+           'nf_monounsaturated_fat': nf_monounsaturated_fat,
+           'nf_cholesterol': nf_cholesterol,
+           'nf_sodium': nf_sodium,
+           'nf_total_carbohydrate': nf_total_carbohydrate,
+           'nf_dietary_fiber': nf_dietary_fiber,
+           'nf_sugars': nf_sugars,
+           'nf_protein': nf_protein,
+           'nf_vitamin_a_dv': nf_vitamin_a_dv,
+           'nf_vitamin_c_dv': nf_vitamin_c_dv,
+           'nf_calcium_dv': nf_calcium_dv,
+           'nf_iron_dv': nf_iron_dv,
+           'nf_potassium': nf_potassium,
+           'nf_servings_per_container': nf_servings_per_container,
+           'nf_serving_size_qty': nf_serving_size_qty,
+           'nf_serving_size_unit': nf_serving_size_unit,
+           'nf_serving_weight_grams': nf_serving_weight_grams,
+           'metric_qty': metric_qty,
+           'metric_uom': metric_uom,
+           'images_front_full_url': images_front_full_url,
+           'item_price': item_price}
         productsForm.append(product)
 
     cursor.execute('SELECT @total_records')
